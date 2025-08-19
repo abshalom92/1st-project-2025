aeb12072e2c3f1419e0320c50287a46cce671520
abshalom92 <abshalom92@gmail.com>
2025-08-18 23:42:28 -0400

    added more meme numbers to trigger the G button, gixed the glitch that allows me to keep typing straight 0s, fixed bck button from generating a 0, added triggerBtnEffects to all buttons.

--- script.js
+++ script.js
@@ -12,28 +12,31 @@
 
     //8/15 must get img to render conditionally in-app & add a link back when picture covers calculator. #1
     // #2 or just an X button. 
+    //4. creating a backend service so that the UI is a very thin client. 
+        // no logic running on the main page, server houses all the JS
+        //will help me understand server/client communication.
 
     const display = document.getElementById("calculator-screen");
 
 function appendToDisplay(input){
-    display.value += input;
+    display.value = display.value === "0" && input === "0" ? "0" : display.value + input;
     console.log();
-
     triggerBtnEffects();
+    //will need to add a fix for handling duplicate periods in the number, duplicate sequential operators
+    //create a message for syntax error
 }
 
 function toggleNegativeVal(){
     display.value *= -1;
+    triggerBtnEffects();
 }
 
 function backSpaceBtn(){
-    let word = `${display.value}` 
+    let word = `${display.value}`
     display.value = word.slice(0,word.length-1);
-    display.value = Number(display.value)
     triggerBtnEffects();
-    
 }
-// when I havve the number + number, that it doesn't treat it like a string. take a look at that slice function  
+
 
 function calculate(input){
     try{
@@ -43,26 +46,25 @@ function calculate(input){
     catch{
     display.value ="error";    
     }
-    
-    
+    triggerBtnEEffects();
 }
-let shouldShowCalc = false;
-
 
 function clearCalcDisplay(){
     display.value = "";
     triggerBtnEffects();
 }
 
-
-
 function triggerBtnEffects(){
     
     const myButtons = document.getElementsByClassName("hiddenBtn");
-    console.log(myButtons);
-
+    console.log("checkpoint outside", myButtons);
+    const disp = display.value;
+    
+    
     for(const button of myButtons){ 
-        if (display.value === "80085"){
+        console.log("checkpoint inside", button)
+        if (disp === "80085" || disp ==="8008135" || disp ==="69" || disp ==="420" || disp ==="69420" || disp ==="80087355" || disp ==="55378008" || disp ==="58008"){
+            console.log("checkpoint triggered")
             button.style.opacity = "1";
             button.style.visibility = "visible";
         } else{
@@ -72,24 +74,29 @@ function triggerBtnEffects(){
     }
 
     console.log("Checkpoint 1 gorillaBtn", display.value);
-
 }
 
 function renderImage(){
-    console.log("image rendered");
+    console.log("image rendered, calculator collapsed");
     const calculatorDiv = document.getElementById("calculator");
     const gorillaImg = document.getElementById("gorillaImg");
+    const exitButton = document.getElementById("exit-button");
 
     gorillaImg.style.display = "block";
     calculatorDiv.style.visibility = "collapse";
 
+    setTimeout(() => {
+    exitButton.classList.add("show");
+    }, 1000);
 }
 
 function renderCalculator(){
-    console.log("calculator rendered");
+    console.log("image exited & calculator rendered");
     const calculatorDiv = document.getElementById("calculator");
     const gorillaImg = document.getElementById("gorillaImg");
-
+    const exitButton = document.getElementById("exit-button")
+    
     gorillaImg.style.display = "none";
     calculatorDiv.style.visibility = "visible";
+    exitButton.classList.remove("show");
 }
